7c51964959f9b2e0dd008645982a3a3d9bd80782
Bala-code-24 <balajisanthanam200@gmail.com>
2025-04-19 07:45:30 +0530

    remove some balls

--- src/constants/index.js
+++ src/constants/index.js
@@ -62,18 +62,6 @@ const services = [
 
 const technologies = [
   {
-    name: "React JS",
-    icon: reactjs,
-  },
-  {
-    name: "Redux Toolkit",
-    icon: redux,
-  },
-  {
-    name: "Tailwind CSS",
-    icon: tailwind,
-  },
-  {
     name: "Node JS",
     icon: nodejs,
   },
